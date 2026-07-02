46857ae82ab7e995f1c60fa85d2b0a767707891c
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2026-07-02 10:05:06 +0100

    review

--- src/app/garfile/supportingdocuments/get.controller.js
+++ src/app/garfile/supportingdocuments/get.controller.js
@@ -38,6 +38,10 @@ module.exports = (req, res) => {
           error = [{ identifier: 'file', message: 'No file selected for upload' }];
           break;
         case 'limit':
+          error = [
+            { message: 'The total file size of all uploaded documents must be less than 8MB', identifier: 'file' },
+          ];
+          break;
         case 'number':
           error = [{ message: '8 is the maximum number of documents that can be uploaded', identifier: 'file' }];
           break;
