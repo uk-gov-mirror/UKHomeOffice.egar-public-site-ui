2cd88341a29dd940d764daecc6471cf106006b9a
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2026-08-08 20:55:00 +0100

    add correlationId to response so it can be captured

--- src/common/utils/correlationContext.js
+++ src/common/utils/correlationContext.js
@@ -3,9 +3,10 @@ const { v4: uuidv4 } = require('uuid');
 
 const correlationContext = new AsyncLocalStorage();
 
-const correlationIdMiddleware = (req, _res, next) => {
+const correlationIdMiddleware = (req, res, next) => {
   const correlationId = uuidv4();
   req.correlationId = correlationId;
+  res.setHeader('X-Correlation-ID', correlationId);
   correlationContext.run({ correlationId }, next);
 };
 
